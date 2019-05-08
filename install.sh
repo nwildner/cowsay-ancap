5d716c873467c32ab3f8cdace93dc28901be9484
nwildner <nicolasgaucho@gmail.com>
2019-05-08 15:25:56 -0300

    Create install.sh
    First stages of cowsay-ancap. Thats why i'm commiting directly to master.

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+#
+# install.sh
+#
+# Instalation script to add
+# anarcho-cows(or sneks)
+#
+# 2019 - Nícolas Wildner
+
+# Cowsay on Debian-based distros is not on root path,
+# neither is installed inside /usr/games
+COWEXISTS="$(command -v cowsay /usr/games/cowsay | head -1)"
+if ! [ -x "$COWEXISTS" ]; then
+        echo 'Error: cowsay is not installed.' >&2
+        exit 1
+fi
+
+if ! [ -w "/usr/share/cowsay/cows/" ]; then
+        echo 'Error: You dont have permission to write cows dir' >&2
+        exit 1
+fi
+
+echo 'Okay, installing animals without prompt'
+install -D $PWD/cows/* /usr/share/cowsay/cows/
